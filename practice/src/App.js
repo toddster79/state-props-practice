0ec08284a1fd457157d4c659ef129fd5b7063dc2
Todd Carlson <todd.carlson3030@me.com>
2020-04-23 11:49:48 -0600

    Second Commit

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Card } from './components/Card'
 
-function App() {
+
+class App extends Component { 
+
+  state = {
+    students: ["Todd", "Bianca", "Jill", "Jared", "David"]
+  }  
+
+  createCards = () => {
+    return this.state.students.map(student => {
+      return <Card name={student}/>
+    })
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {this.createCards()}
+      </ul>
     </div>
-  );
+  ); 
+  }
 }
 
 export default App;
---
+++ practice/src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Card = (props) => {
+    return (
+    <li>{props.name}</li>
+    )
+} 
+
+export default Card 
